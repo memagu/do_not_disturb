76b289997f85be9595699bbc87f7c6eca7dd7b6b
Melker Widén <melker.widen@gmail.com>
2024-01-18 21:00:34 +0100

    Update __init__.py

--- src/audio/__init__.py
+++ src/audio/__init__.py
@@ -1,8 +1,6 @@
-from . import constants
-
 from .audio_processing import NoiseLevel
 from .audio_processing import bytes_to_dbfs, rms, rms_to_dbfs
 
 from .audio_stream import AudioStream
 
-
+from .speech_recognition import SpeechRecognizer
