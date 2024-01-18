87cf009028ac0b92e590d8acea0ba674c679783e
Melker Widén <melker.widen@gmail.com>
2024-01-18 20:59:09 +0100

    Refactor, add more typing

--- src/audio/audio_stream.py
+++ src/audio/audio_stream.py
@@ -4,17 +4,17 @@ import pyaudio
 
 
 class AudioStream:
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         self.args = args
         self.kwargs = kwargs
         self.audio_interface = pyaudio.PyAudio()
         self.stream: Optional[pyaudio.PyAudio.Stream] = None
 
-    def __enter__(self):
+    def __enter__(self) -> pyaudio.Stream:
         self.stream = self.audio_interface.open(*self.args, **self.kwargs)
         return self.stream
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type, exc_val, exc_tb) -> None:
         self.stream.stop_stream()
         self.stream.close()
         self.audio_interface.terminate()
